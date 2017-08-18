8591c661b0eaebbc8d76f463547ad8b3a7b8fac3
chris-bacon <chris.bacon@geekology.ltd.uk>
2017-08-18 16:17:08 +0100

    Started class and composer

---
+++ src/PHPUBG/PHPUBG.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PHPUBG;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+class PHPUBG {
+
+    public function __construct($apiToken, $pubgNickname)
+    {
+        $this->apiToken = $apiToken;
+        $this->guzzle = new \GuzzleHttp\Client();
+        $this->pubgURL = "https://pubgtracker.com/api/profile/pc/$pubgNickname";
+    }
+
+    /**
+     *
+     */
+    public function get()
+    {
+        return $this->guzzle->request('GET', $this->pubgURL, [
+            "headers" => [
+                "TRN-Api-Key" => $this->apiToken
+            ]
+        ]);
+    }
+
+    /**
+     * Takes a guzzle response, gets the body, and converts it to an array
+     * @param Guzzle Response
+     * @return Array
+     */
+    public function readBody($guzzleResponse)
+    {
+        return json_decode($guzzleResponse->getBody(), true);
+    }
+}
